0237fd17b87b6cafbdfb7b1af9580afd1458cb2f
HarshSharma1246 <142594711+HarshSharma1246@users.noreply.github.com>
2023-11-29 15:04:30 +0530

    Update SnakeAndLadder.java
    In this update, more functions are added to the game:
    1. UI has improved (Denoting all the variables by their colors (red for snake, green for ladder, Player1 and Player 2 has blue and yellow color) on the terminal)
    2. Some additional features are added in the dice. (Gives turn again to the same player if he/she gets 6)
    3. Shows the dice value and movement of the P1 and P2 on the terminal on each move

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,24 +1,35 @@
-
-import java.util.Random;
 import java.util.Scanner;
 
 public class SnakeAndLadder {
+    public static final String COLOR_RESET = "\u001B[0m";
+    public static final String Player1_YELLOW = "\u001B[33m";
+    public static final String Player2_BLUE = "\u001B[34m";
+    public static final String SNAKE_RED = "\u001B[31m";
+    public static final String LADDER_GREEN = "\u001B[32m";
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
 
-        int numSnakes = random.nextInt(10) + 5;
-        int numLadders = random.nextInt(10) + 5;
+        int numSnakes = (int) Math.floor(Math.random() * 5) + 7;
+        int numLadders = (int) Math.floor(Math.random() * 5) + 7;
 
-        int[] snakes = new int[numSnakes];
-        int[] ladders = new int[numLadders];
+        int[][] snakes = new int[numSnakes][2]; // Zeroth index contains position of snake and at 1st index contains
+                                                // length of snake
+        int[][] ladders = new int[numLadders][2]; // Same as snake array
 
         for (int i = 0; i < numSnakes; i++) {
-            snakes[i] = random.nextInt(90) + 10;
+            snakes[i][0] = (int) Math.floor(Math.random() * 80) + 20; // Gives the snake position higher than 20 always
+            snakes[i][1] = (int) Math.floor(Math.random() * (snakes[i][0] - 10)) + 10;
+            // Snake length is always greater than 10 and is also dependent on the position
+            // of the snake
+            // Ex. if snake position is 30, snake length can be from 10 to 29
         }
 
         for (int i = 0; i < numLadders; i++) {
-            ladders[i] = random.nextInt(90) + 10;
+            ladders[i][0] = (int) Math.floor(Math.random() * 70) + 10; // Ladders only remain from 10th position to 80
+            ladders[i][1] = (int) Math.floor(Math.random() * (90 - ladders[i][0])) + 10;
+            // Ladder length is always greater than 10
+            // If ladder position is 70, ladder length can be from 10 to 29
         }
 
         int currentPlayer = 1;
@@ -26,96 +37,141 @@ public class SnakeAndLadder {
         int player2Position = 1;
 
         updateBoard(player1Position, player2Position, snakes, ladders);
-
+        boolean again = false; // Check wheather a player got 6 in last turn, if yes then player will get new
+                               // chance
         while (player1Position < 100 && player2Position < 100) {
-            System.out.println("Press Enter for Player " + currentPlayer + "'s turn...");
-            scanner.nextLine();
 
-            int diceRoll = random.nextInt(6) + 1;
+            if (!again)
+                System.out.println("Press Enter for Player " + currentPlayer + "'s turn...");
+            again = false;
+            sc.nextLine();
+
+            int diceRoll = (int) Math.floor(Math.random() * 6) + 1;
 
             if (currentPlayer == 1) {
+                if (diceRoll + player1Position > 100) {
+                    System.out.println("Player1 can't go beyond 100!! Try again in your next turn");
+                    currentPlayer = 2;
+                    updateBoard(player1Position, player2Position, snakes, ladders);
+                    continue;
+                }
                 player1Position += diceRoll;
-                for (int snake : snakes) {
-                    if (player1Position == snake) {
-                        player1Position -= random.nextInt(10) + 1;
+                System.out.println("Player1 got " + diceRoll + " on dice and went from " + (player1Position - diceRoll)
+                        + " to " + player1Position);
+                for (int[] snake : snakes) {
+                    if (player1Position == snake[0]) {
+                        int temp = player1Position;
+                        player1Position -= snake[1];
+                        System.out.println(
+                                "Oops!! Player1 is eaten by the snakes and went from " + temp + " to "
+                                        + player1Position);
                         break;
                     }
                 }
-                for (int ladder : ladders) {
-                    if (player1Position == ladder) {
-                        player1Position += random.nextInt(10) + 1;
+                for (int ladder[] : ladders) {
+                    if (player1Position == ladder[0]) {
+                        int temp = player1Position;
+                        player1Position += ladder[1];
+                        System.out.println(
+                                "Yeah!! Player1 has taken the ladder and went from " + temp + " to " + player1Position);
                         break;
                     }
+                }
+                if (diceRoll == 6) {
+                    currentPlayer = 1;
+                    again = true;
                 }
                 currentPlayer = 2;
             } else {
+                if (diceRoll + player2Position > 100) {
+                    System.out.println("Player2 can't go beyond 100!! Try again in your next turn");
+                    updateBoard(player1Position, player2Position, snakes, ladders);
+                    currentPlayer = 1;
+                    continue;
+                }
                 player2Position += diceRoll;
-                for (int snake : snakes) {
-                    if (player2Position == snake) {
-                        player2Position -= random.nextInt(10) + 1;
+                System.out.println("Player2 got " + diceRoll + " on dice and went from " + (player2Position - diceRoll)
+                        + " to " + player2Position);
+                for (int snake[] : snakes) {
+                    if (player2Position == snake[0]) {
+                        int temp = player2Position;
+                        player2Position -= snake[1];
+                        System.out.println(
+                                "Oops!! Player2 has eaten by the snake and went from " + temp + " to "
+                                        + player2Position);
                         break;
                     }
                 }
-                for (int ladder : ladders) {
-                    if (player2Position == ladder) {
-                        player2Position += random.nextInt(10) + 1;
+                for (int ladder[] : ladders) {
+                    if (player2Position == ladder[0]) {
+                        int temp = player2Position;
+                        player2Position += ladder[1];
+                        System.out.println(
+                                "Yeah!! Player2 has taken the ladder and went from " + temp + " to " + player2Position);
                         break;
                     }
                 }
-                currentPlayer = 1;
+                if (diceRoll == 6) {
+                    currentPlayer = 2;
+                    again = true;
+                } else
+                    currentPlayer = 1;
             }
+            sc.nextLine();
+            if (!again)
+                updateBoard(player1Position, player2Position, snakes, ladders);
 
-            updateBoard(player1Position, player2Position, snakes, ladders);
-
-            if (player1Position >= 100) {
+            if (player1Position == 100) {
                 System.out.println("Player 1 wins!");
                 break;
-            } else if (player2Position >= 100) {
+            } else if (player2Position == 100) {
                 System.out.println("Player 2 wins!");
                 break;
             }
+            if (again)
+                System.out.println("Roll the dice again!! as you got 6");
         }
     }
 
-    private static void updateBoard(int player1Position, int player2Position, int[] snakes, int[] ladders) {
+    private static void updateBoard(int player1Position, int player2Position, int[][] snakes, int[][] ladders) {
         System.out.print("\033[H\033[2J"); // Clear terminal screen
         System.out.flush();
 
-        System.out.println("===================================");
-        int position = 1;
+        System.out.println("=================================================");
+        int position = 100;
         for (int row = 9; row >= 0; row--) {
             for (int col = 1; col <= 10; col++) {
                 boolean isSnakeOrLadder = false;
-                String symbol = "   ";
-                for (int snake : snakes) {
-                    if (position == snake) {
-                        symbol = " S ";
+                String symbol = "    ";
+                for (int snake[] : snakes) {
+                    if (position == snake[0]) {
+                        symbol = SNAKE_RED + "  S  " + COLOR_RESET;
                         isSnakeOrLadder = true;
                         break;
                     }
                 }
                 if (!isSnakeOrLadder) {
-                    for (int ladder : ladders) {
-                        if (position == ladder) {
-                            symbol = " L ";
+                    for (int ladder[] : ladders) {
+                        if (position == ladder[0]) {
+
+                            symbol = LADDER_GREEN + "  L  " + COLOR_RESET;
                             isSnakeOrLadder = true;
                             break;
                         }
                     }
                 }
                 if (!isSnakeOrLadder) {
-                    symbol = String.format("%3d", position);
+                    symbol = String.format(" %3d ", position);
                 }
                 if (position == player1Position) {
-                    symbol = " P1 ";
+                    symbol = Player1_YELLOW + "  P1  " + COLOR_RESET;
                 } else if (position == player2Position) {
-                    symbol = " P2 ";
+                    symbol = Player2_BLUE + "  P2  " + COLOR_RESET;
                 }
                 System.out.print(symbol);
-                position++;
+                position--;
             }
             System.out.println();
+            System.out.println();
+
         }
-        System.out.println("===================================");
-    }
-}
